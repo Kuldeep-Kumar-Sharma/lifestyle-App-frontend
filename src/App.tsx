1d970d4aa35b20adcacea30c20c0f84b56e8f693
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2025-01-08 23:04:10 +0530

    plan of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 
 function App() {
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar bg="primary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+        <Navbar.Brand href="#home">Life Style App</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#logevents">LogEvents</Nav.Link>
+          <Nav.Link href="#logemotions">LogEmotions</Nav.Link>
+          <Nav.Link href="#dailyJournal">Daily Journal</Nav.Link>
+          <Nav.Link href="#dailyPlan">Daily Plan</Nav.Link>
+          <Nav.Link href="#Longterm Goal">Long Term Goals</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
