976ac49e2b47a6be5b20174388f3aa00217ce0c8
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2025-04-20 23:57:23 +0200

    starting journal component

--- src/Pages/DailyJournal/index.tsx
+++ src/Pages/DailyJournal/index.tsx
@@ -1,8 +1,30 @@
 import Container from "react-bootstrap/Container";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 import "./index.css";
 
 function DailyJournal() {
-  return <Container>Here there would be editor with current date , with plugin editor, encrypt when sent to backend, you cannot change the date here</Container>;
+  return (
+    <Container>
+      Here there would be editor with current date , with plugin editor, encrypt
+      when sent to backend, you cannot change the date here{" "}
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          Tab content for Home
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          Tab content for Profile
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          Tab content for Contact
+        </Tab>
+      </Tabs>
+    </Container>
+  );
 }
 
 export default DailyJournal;
