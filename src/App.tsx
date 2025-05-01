8ab3334bf865197a6f15f624601fea33ca1e065e
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2025-05-01 03:01:41 +0200

    adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import "./App.css";
+import LogEvents from "./Pages/LogEvents";
+import LogEmotions from "./Pages/LogEmotions";
+import DailyJournal from "./Pages/DailyJournal";
+import Plan from "./Pages/Plan";
+import LongtermGoal from "./Pages/LongtermGoal";
 
 function App() {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/logevents" element={<LogEvents />}></Route>
+        <Route path="/logemotions" element={<LogEmotions />}></Route>
+        <Route path="/dailyJournal" element={<DailyJournal />}></Route>
+        <Route path="/planning" element={<Plan />}></Route>
+        <Route path="/longtermGoal" element={<LongtermGoal />}></Route>
+      </Routes>
       <Container>
         <Navbar.Brand href="#home">Life Style App</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#home">
+            <Route path="/" element={<App />}>
+              Home
+            </Route>
+          </Nav.Link>
           <Nav.Link href="#logevents">Log Events</Nav.Link>
           <Nav.Link href="#logemotions">Log Emotions</Nav.Link>
           <Nav.Link href="#dailyJournal">Daily Journal</Nav.Link>
